0327f37c767337e4735876ebdf6ab80a0e3aa512
Humair234 <121742551+Humair234@users.noreply.github.com>
2023-02-12 06:48:59 +0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','kruxton')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','humair234','humair2349','kruxton')or die("Could not connect to mysql".mysqli_error($con));
